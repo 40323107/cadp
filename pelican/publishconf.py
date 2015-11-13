356c13ddfba7da053983f2e479caa3900f1ad591
40323107 <40323107@gm.nfu.edu.tw>
2015-11-14 02:01:41 +0800

    add new 11/14

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = 'KMOL'
-SITENAME = '2015FALL CADP 課程'
+AUTHOR = '40323107'
+SITENAME = '2015 FALL CADP  課程'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,17 +26,15 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),
-         ('40323107個人網頁','file:///V:/tmp/cadp/category/python.html'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python', 'http://python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('github', 'http://github.com/ametaireau'),)
+          
 DEFAULT_PAGINATION = 10
 
-SITEURL = 'http://example.com'
+SITEURL = 'http://coursemdetw.github.io/reveal'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -47,5 +45,19 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-DISQUS_SITENAME = "2015Fall"
+DISQUS_SITENAME = "2015fall"
 #GOOGLE_ANALYTICS = ""
+
+# 必須絕對目錄或相對於設定檔案所在目錄
+PLUGIN_PATHS = ['./../plugin']
+PLUGINS = ['liquid_tags.notebook']
+# 目錄設定相對於 reveal 下的 content 目錄
+NOTEBOOK_DIR = 'notebook'
+'''
+if not os.path.exists('_nb_header.html'):     
+    import warnings 
+    warnings.warn("_nb_header.html not found.") 
+else: 
+    EXTRA_HEADER = open('_nb_header.html', encoding="utf-8").read()
+'''
+
